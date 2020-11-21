531bd90e255de6a8be2fbfee59c671f70dec4f6c
Ryan Allred <ryanallredxyz@gmail.com>
2020-11-20 18:26:18 -0700

    add jsonld

--- ghostdogcatering/src/pages/_app.js
+++ ghostdogcatering/src/pages/_app.js
@@ -13,6 +13,33 @@ export default class MyApp extends App {
             href="https://fonts.googleapis.com/css2?family=Montserrat&family=Teko:wght@500&display=swap"
             rel="stylesheet"
           />
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: `
+{
+  "@context": "https://schema.org",
+  "@type": "Restaurant",
+  "name": "Ghost Dog Catering",
+  "image": "https://ghostdogcatering.com/logo.png",
+  "@id": "",
+  "url": "https://ghostdogcatering.com",
+  "telephone": "",
+  "menu": "https://ghostdogcatering.com",
+  "servesCuisine": "",
+  "acceptsReservations": "false",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "",
+    "addressLocality": "Eagle Mountain",
+    "addressRegion": "UT",
+    "postalCode": "84005",
+    "addressCountry": "US"
+  }  
+}
+          `,
+            }}
+          />
         </Head>
         <Provider>
           <Component {...pageProps} />
